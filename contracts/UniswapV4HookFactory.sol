d6df7c70d4ee92b85559be031ea37bc203dae327
Berbex <berbesidaniel@gmail.com>
2025-06-12 21:06:17 -0400

    feat: minor improvements

--- contracts/UniswapV4HookFactory.sol
+++ contracts/UniswapV4HookFactory.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.27;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
-import "./ZeroILSwapSamePoolHook.sol";
+import {ZeroILSwapSamePoolHook} from "./ZeroILSwapSamePoolHook.sol";
 
 /**
  * @title Factory for deterministic deployment of Uniswap v4 Hooks
@@ -17,6 +17,7 @@ import "./ZeroILSwapSamePoolHook.sol";
  */
 contract UniswapV4HookFactory is Ownable {
     using Hooks for IHooks;
+
     event Deployed(address deployedHook);
 
     constructor() Ownable(_msgSender()) {}
@@ -25,7 +26,7 @@ contract UniswapV4HookFactory is Ownable {
      * @notice Deploy a Hook with constructor arguments
      * @param hookConstructorArguments abi-encoded arguments to the Hook constructor
      * @param salt This param should be calculated so that the deployed Hook address is correct (see `Hooks.validateHookAddress()` in `@uniswap/v4-core/contracts/libraries/Hooks.sol`)
-     * @return address of deployed Hook
+     * @return Address of deployed Hook
      * @dev If Hook extends BaseHook, its constructor verifies the address to follow rules
      */
     function deploy(bytes calldata hookBytecode, bytes calldata hookConstructorArguments, bytes32 salt) external onlyOwner returns (address) {
@@ -39,10 +40,10 @@ contract UniswapV4HookFactory is Ownable {
     /**
      * @notice Compute address of the Hook with constructor arguments
      * @dev This function should be used to find salt, that generates correct address
-     * @param hookBytecode Bytecode of the Hook (is Solidity this is `type(contractName).creationCode`)
+     * @param hookBytecode Bytecode of the Hook (in Solidity this is `type(contractName).creationCode`)
      * @param hookConstructorArguments abi-encoded arguments to the Hook constructor
      * @param salt This param should be calculated so that the deployed Hook address is correct (see `Hooks.validateHookAddress()` in `@uniswap/v4-core/contracts/libraries/Hooks.sol`)
-     * @return computed address of the Hook
+     * @return Computed address of the Hook
      */
     function computeAddress(bytes calldata hookBytecode, bytes calldata hookConstructorArguments, bytes32 salt) external view returns (address) {
         bytes memory deployBytecode = _computeBytecode(hookBytecode, hookConstructorArguments);
