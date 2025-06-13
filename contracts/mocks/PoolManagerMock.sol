293fbc52c643dba63c08d45219e0878016eb6ff8
Berbex <berbesidaniel@gmail.com>
2025-06-12 20:43:35 -0400

    feat: remove unnecessary mocks

--- contracts/mocks/PoolManagerMock.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
-
-import {PoolManager} from "@uniswap/v4-core/src/PoolManager.sol";
-
-contract PoolManagerMock is PoolManager {
-    constructor(address initialOwner) PoolManager(initialOwner) {}
-}
