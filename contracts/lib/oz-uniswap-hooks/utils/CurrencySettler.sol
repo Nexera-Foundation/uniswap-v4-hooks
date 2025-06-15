16d20588f1c56ccfb29c9fb2fb42bc5f490dbcb6
Berbex <berbesidaniel@gmail.com>
2025-06-15 13:00:48 -0400

    fix: wrong import path

--- contracts/lib/oz-uniswap-hooks/utils/CurrencySettler.sol
+++ contracts/lib/oz-uniswap-hooks/utils/CurrencySettler.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 
 import {Currency} from "@uniswap/v4-core/src/types/Currency.sol";
 import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {IERC20} from "@openzeppelin/contracts//token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
